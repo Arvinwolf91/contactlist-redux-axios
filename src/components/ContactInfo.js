8bd5348e05b4f169bdec124c3ce46e17ab07a3bf
arvin.sanmuga <arvin_wolf@Arvins-MacBook-Pro-2.local>
2018-02-23 12:05:01 +0800

    [] Styling ContactInfo

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -2,17 +2,31 @@
 
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import CardSection from './common/CardSection';
 
 class ContactInfo extends Component {
 
     render() {
         return (
             <View>
+            <CardSection style={{ borderBottomWidth: 0 }}>
+            <Text style={styles.InfoTitleStyles}> Email: </Text>
             <Text> { this.props.contact.email } </Text>
+            </CardSection>
+            <CardSection>
+            <Text style={styles.InfoTitleStyles}> Gender: </Text>
+            <Text> { this.props.contact.gender } </Text>
+            </CardSection>
             </View>
     );
     }
 }
+
+const styles = {
+    InfoTitleStyles: {
+        fontWeight: 'bold'
+    }
+};
 
 // How to pop view ?
 // import { Actions } from 'react-native-router-flux';
